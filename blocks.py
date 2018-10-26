4c0fa9eee0ac77b56223cd4accbbd71234a688e9
Gargi Maheshwari <gargimaheshwari@users.noreply.github.com>
2018-10-26 23:38:55 +0530

    Update blocks.py
    Commented.
    Fixed bugs.

--- blocks.py
+++ blocks.py
@@ -17,13 +17,13 @@ W = 40 # Width
 H = 30 # Height
 N = 100 # number of blocks
 
-blocks = deque()
+blocks = deque() # stores the locations of the blocks
 
 #colours for the background and the squares
 BLACK = Color("black")
 WHITE = Color("white")
    
-def draw_rects(window):
+def draw_rects(window): # This function draws only new squares and sends them to pygame update function
     
     dirty_rects = []
     
@@ -31,8 +31,8 @@ def draw_rects(window):
         
         x_pos = ((W if (pos%W) == 0 else (pos%W))-1)*D + D/2
         y_pos = int(pos/W)*D + D/2
-        dirty_rects.append(pygame.draw.rect(window, black, (x_pos, y_pos, D, D), 1))
-        dirty_rects.append(pygame.draw.rect(window, white, (x_pos +1, y_pos+ 1, D-2, D-2)))
+        dirty_rects.append(pygame.draw.rect(window, BLACK, (x_pos, y_pos, D, D), 1))
+        dirty_rects.append(pygame.draw.rect(window, WHITE, (x_pos +1, y_pos+ 1, D-2, D-2)))
         
     return(dirty_rects)
 
@@ -41,11 +41,10 @@ def main():
     pygame.init()
     window = pygame.display.set_mode((D*W + D, D*H + D))
     pygame.display.set_caption("blocks on a screen")
-
-    blocks.append(random.randint(1, W*H))
     
-    pygame.draw.rect(window, white, (0, 0, W*D + D, H*D + D))
-    pygame.draw.rect(window, black, (int(D/2), int(D/2), W*D, H*D))    
+    # Draw the screen
+    pygame.draw.rect(window, WHITE, (0, 0, W*D + D, H*D + D))
+    pygame.draw.rect(window, BLACK, (int(D/2), int(D/2), W*D, H*D))    
     pygame.display.update()
     
     clock = pygame.time.Clock()
@@ -53,7 +52,7 @@ def main():
     for i in range(N-1):
         possible_box = set(range(W*H)) - set(blocks)
         blocks.append(random.choice(list(possible_box)))
-        pygame.display.update(draw_rects(window))
+        pygame.display.update(draw_rects(window)) # Update only to draw squares
         clock.tick(30)
     
     time.sleep(0.3)
