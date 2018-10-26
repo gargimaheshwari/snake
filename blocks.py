dadfc01be139fe4e3f74473d26464f0e518d7fc3
Gargi Maheshwari <gargimaheshwari@users.noreply.github.com>
2018-10-26 23:33:51 +0530

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -11,47 +11,48 @@ from queue import deque
 from pygame.color import Color
 import time
 
-d = 10
-w = 40
-h = 30
-n = 100
+# Dimensions of the screen
+D = 10 # resolution
+W = 40 # Width
+H = 30 # Height
+N = 100 # number of blocks
 
-snake = deque()
+blocks = deque()
 
-black = Color("black")
-white = Color("white")
+#colours for the background and the squares
+BLACK = Color("black")
+WHITE = Color("white")
    
 def draw_rects(window):
     
     dirty_rects = []
     
-    for i, pos in enumerate(snake):
+    for i, pos in enumerate(blocks):
         
-        x_pos = ((w if (pos%w) == 0 else (pos%w))-1)*d + d/2
-        y_pos = int(pos/w)*d + d/2
-        dirty_rects.append(pygame.draw.rect(window, black, (x_pos, y_pos, d, d), 1))
-        dirty_rects.append(pygame.draw.rect(window, white, (x_pos +1, y_pos+ 1, d-2, d-2)))
+        x_pos = ((W if (pos%W) == 0 else (pos%W))-1)*D + D/2
+        y_pos = int(pos/W)*D + D/2
+        dirty_rects.append(pygame.draw.rect(window, black, (x_pos, y_pos, D, D), 1))
+        dirty_rects.append(pygame.draw.rect(window, white, (x_pos +1, y_pos+ 1, D-2, D-2)))
         
     return(dirty_rects)
 
 def main():
     
     pygame.init()
-    window = pygame.display.set_mode((d*w + d, d*h + d))
+    window = pygame.display.set_mode((D*W + D, D*H + D))
     pygame.display.set_caption("blocks on a screen")
 
-    snake.append(random.randint(1, w*h))
+    blocks.append(random.randint(1, W*H))
     
-    pygame.draw.rect(window, white, (0, 0, w*d+d, h*d+d))
-    pygame.draw.rect(window, black, (int(d/2), int(d/2), w*d,h*d))
-    
+    pygame.draw.rect(window, white, (0, 0, W*D + D, H*D + D))
+    pygame.draw.rect(window, black, (int(D/2), int(D/2), W*D, H*D))    
     pygame.display.update()
     
     clock = pygame.time.Clock()
     
-    for i in range(n):
-        possible_box = set(range(w*h)) - set(snake)
-        snake.append(random.choice(list(possible_box)))
+    for i in range(N-1):
+        possible_box = set(range(W*H)) - set(blocks)
+        blocks.append(random.choice(list(possible_box)))
         pygame.display.update(draw_rects(window))
         clock.tick(30)
     
